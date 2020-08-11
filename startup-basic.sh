ea07670bbf7cd9b185ee57076c4654ee11dbd973
baahk01 <kodjo.baah@bbc.co.uk>
2020-08-11 09:06:36 +0100

    removed

--- startup-basic.sh
+++ startup-basic.sh
@@ -3,7 +3,7 @@
 
 HEAP="-Xms1g -Xmx1g -XX:MaxMetaspaceSize=512m"
 export _JAVA_OPTIONS="-Xms512m -Xmx1024m"
-JAVA_OPTIONS="-Djavax.net.ssl.keyStoreType=pkcs12 -Djavax.net.ssl.keyStore=/Users/baahk01/workspace/bbc_cert.p12 -Djavax.net.ssl.keyStorePassword=vagrant"
+JAVA_OPTIONS="-Djavax.net.ssl.keyStoreType=pkcs12 -Djavax.net.ssl.keyStore=/Users/baahk01/workspace/bbc_cert.p12 -Djavax.net.ssl.keyStorePassword=xxxxx"
 
 $JMETER_HOME/bin/jmeter.sh  -N 127.0.0.1 $JAVA_OPTIONS -t src/test/upload/upload.jmx
 
